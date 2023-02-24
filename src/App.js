b59f6c719ed495a606e5aaf0cc5b4aebb3377e49
Ben Kitamura <ben.kitamura@gmail.com>
2023-02-24 13:35:51 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -23,27 +23,27 @@ class App extends Component {
       box: {},
       route: "signin",
       isSignedIn: false,
-      // user: {
-      //   id: "",
-      //   name: "",
-      //   email: "",
-      //   entries: 0,
-      //   joined: "",
-      // },
+      user: {
+        id: "",
+        name: "",
+        email: "",
+        entries: 0,
+        joined: "",
+      },
     };
   }
 
-  // loadUser = (data) => {
-  //   this.setState({
-  //     user: {
-  //       id: data.id,
-  //       name: data.name,
-  //       email: data.email,
-  //       entries: data.entries,
-  //       joined: data.joined,
-  //     },
-  //   });
-  // };
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (data) => {
     const clarifaiFace =
@@ -69,20 +69,6 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-
-    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
-    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
-    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
-    // If that isn't working, then that means you will have to wait until their servers are back up.
-
-    // Old Way:
-    // app.models
-    //   .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    //   .then((response) =>
-    //     this.displayFaceBox(this.calculateFaceLocation(response))
-    //   )
-    //   .catch((err) => console.log(err));
-    // New Way:
     app.models
       .predict(
         {
@@ -98,9 +84,9 @@ class App extends Component {
           fetch("http://localhost:3000/image", {
             method: "put",
             headers: { "Content-Type": "application/json" },
-            // body: JSON.stringify({
-            //   id: this.state.user.id,
-            // }),
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
           })
             .then((response) => response.json())
             .then((count) => {
@@ -140,8 +126,9 @@ class App extends Component {
           <div>
             <Logo />
             <Rank
-            // name={this.state.user.name}
-            // entries={this.state.user.entries}
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+              onRouteChange={this.onRouteChange}
             />
             <ImageLinkForm
               onInputChange={this.onInputChange}
@@ -150,9 +137,12 @@ class App extends Component {
             <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         ) : route === "signin" ? (
-          <Signin onRouteChange={this.onRouteChange} />
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
         ) : (
-          <Register onRouteChange={this.onRouteChange} />
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
         )}
       </div>
     );
